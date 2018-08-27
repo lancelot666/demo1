7ce55c483d998e9923d0f399d656d5f6ba69f77b
Lancelot666 <249930369@qq.com>
2018-08-27 16:07:10 +0800

    1111

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
-import {get,post} from './config/fetchAPI';
-
+import {connect} from 'react-redux';
+import * as act from './redux/actions/homeAction';
 class App extends Component {
-  constructor(){
-    super();  
+  constructor(props){
+    super(props);  
     console.log('初始化')
-    this.getData();
-    
+    //this.getData();
+    console.log(this.props)
   }
   async getData(){
-    //await get();
-    await post('',{id:1});
+    await window.get();
+    await window.post('',{id:1});
+  }
+  changeName2(b){
+    this.props.mapCHANGENAME(b)
   }
   render() {
     return (
       <div className="App">
+        <div onClick={this.props.mapCHANGENAME.bind(this,'444')}>{this.props.mapName}</div>
+        <div onClick={this.changeName2.bind(this,'555')}>{this.props.mapName}</div>
       </div>
     );
   }
 }
-
-export default App;
+const mapState = (state)=>{
+  var info = state.homeData;
+  //console.log('state:',state)
+  return {
+    mapName:info.name//对应本组件props需要的属性products
+  }
+}
+const mapDispatch = (dispatch)=>{
+  return {
+    mapCHANGENAME: (v) => dispatch(act.CHANGENAME(v))
+  }
+}
+export default connect(mapState,mapDispatch)(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -7,6 +9,12 @@ import {logSwitch} from './config/config';
 import 'whatwg-fetch';
 import 'es6-promise';
 import {get,post} from './config/fetchAPI';
-logSwitch();
-ReactDOM.render(<App />, document.getElementById('root'));
+window.get = get;
+window.post = post;
+logSwitch();    //实例打印开关
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
